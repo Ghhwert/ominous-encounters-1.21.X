17c75d11e1f6721399283506e841ef9f03214381
Ghhwert <Clockejames@gmail.com>
2026-03-06 10:03:35 +0100

    Initial commit

---
+++ src/main/java/net/ghhwert/ominousencounters/OminousEncounters.java
@@ -0,0 +1,17 @@
+package net.ghhwert.ominousencounters;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class OminousEncounters implements ModInitializer {
+	public static final String MOD_ID = "ominous_encounters";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+
+	}
+}
---
+++ src/main/java/net/ghhwert/ominousencounters/OminousEncountersClient.java
@@ -0,0 +1,10 @@
+package net.ghhwert.ominousencounters;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OminousEncountersClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
